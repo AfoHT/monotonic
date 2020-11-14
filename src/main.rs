8d98d5398b58778adcb0a5e59ec0ef54bbcdbeef
Henrik Tjäder <henrik@tjaders.com>
2020-11-14 17:22:48 +0000

    Implement the UI Korken suggested

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,10 @@ pub use monotonic::monotonic;
 fn main() {
     println!("Hello, monotonic!");
 
-    #[allow(dead_code)]
-    #[monotonic(
-        interruptname=ISR, handlerimpl=handler
-        interruptname=ISR, handlerimpl=handler
-        interruptname=ISR, handlerimpl=handler
-        interruptname=ISR, handlerimpl=handler
-        interruptname=ISR, handlerimpl=handler
-        )]
-    struct Monotonic {}
+    struct MonotonicImpl {
+    }
 
     #[allow(dead_code)]
-    #[monotonic(
-        monotonic = [ISR, handler]
-        monotonic = [ISR, handler]
-        )]
-    struct Monotonic2 {}
+    #[monotonic(binds = SysTick)]
+    type MyMonotonic = MonotonicImpl;
 }
